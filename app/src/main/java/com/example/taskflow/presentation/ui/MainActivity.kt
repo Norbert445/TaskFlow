ae907cf2171452d23617ff5d148df3e747f61972
Norbert445 <norbimacsali445@gmail.com>
2025-03-31 14:21:01 +0200

    feat: show todos + theming

--- app/src/main/java/com/example/taskflow/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskflow/presentation/ui/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.taskflow.presentation.ui.theme.TaskFlowTheme
+import com.example.taskflow.presentation.ui.todos.TodosScreen
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +21,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    contentColor = MaterialTheme.colorScheme.surfaceContainer,
+                    contentWindowInsets = WindowInsets.safeDrawing,
+                ) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController,
+                        startDestination = Todos,
+                    ) {
+                        composable<Todos> { TodosScreen(innerPadding) }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TaskFlowTheme {
-        Greeting("Android")
-    }
+    @Serializable
+    object Todos
 }
