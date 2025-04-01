5358ff4bb4a09a94c6f2355ca5155461fb5620ab
Norbert445 <norbimacsali445@gmail.com>
2025-04-01 07:54:03 +0200

    refactor: optimize imports

--- app/src/main/java/com/example/taskflow/presentation/ui/todos/TodosScreen.kt
+++ app/src/main/java/com/example/taskflow/presentation/ui/todos/TodosScreen.kt
@@ -3,13 +3,10 @@ package com.example.taskflow.presentation.ui.todos
 import TodoItem
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
