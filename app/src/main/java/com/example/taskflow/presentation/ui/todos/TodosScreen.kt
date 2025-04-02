a695b16f3f5e4871a6b47d851706f1b1d8f063d9
Norbert445 <norbimacsali445@gmail.com>
2025-04-02 10:08:10 +0200

    feat: fixed todo list tile

--- app/src/main/java/com/example/taskflow/presentation/ui/todos/TodosScreen.kt
+++ app/src/main/java/com/example/taskflow/presentation/ui/todos/TodosScreen.kt
@@ -33,89 +33,91 @@ fun TodosScreen(
 ) {
     val todos = todoViewModel.todos
 
-    if (todos.value.isEmpty()) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp, end = 16.dp, start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(R.drawable.ic_no_data),
-                contentDescription = "No data image",
+            Text(
+                "My Todo List",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineSmall,
             )
-            Text("No todos", color = MaterialTheme.colorScheme.onBackground)
+
+
+            IconButton(onClick = {
+                onDarkModeToggle()
+            }) {
+                Icon(
+                    painter = if (darkModeEnabled.value) painterResource(R.drawable.ic_light_mode) else painterResource(
+                        R.drawable.ic_dark_mode
+                    ),
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = "Toggle light/dark mode",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
         }
-    } else {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(horizontal = 16.dp)
-        ) {
-            item {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        "My Todo List",
-                        color = MaterialTheme.colorScheme.primary,
-                        style = MaterialTheme.typography.headlineSmall,
-                        modifier = Modifier.padding(top = 24.dp, bottom = 12.dp)
-                    )
 
+        if (todos.value.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_no_data),
+                    contentDescription = "No data image",
+                )
+                Text("No todos", color = MaterialTheme.colorScheme.onBackground)
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(horizontal = 16.dp)
+            ) {
+                val (incompleteTodos, completedTodos) = todos.value.partition { !it.isDone }
 
-                    IconButton(onClick = {
-                        onDarkModeToggle()
-                    }) {
-                        Icon(
-                            painter = if (darkModeEnabled.value) painterResource(R.drawable.ic_light_mode) else painterResource(
-                                R.drawable.ic_dark_mode
-                            ),
-                            tint = MaterialTheme.colorScheme.primary,
-                            contentDescription = "Toggle light/dark mode",
-                            modifier = Modifier.size(24.dp)
+                if (incompleteTodos.isNotEmpty()) {
+                    item {
+                        Text(
+                            "Tasks to Do",
+                            style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.secondary),
+                            modifier = Modifier.padding(vertical = 8.dp)
                         )
                     }
-                }
-            }
 
-            val (incompleteTodos, completedTodos) = todos.value.partition { !it.isDone }
-
-            if (incompleteTodos.isNotEmpty()) {
-                item {
-                    Text(
-                        "Tasks to Do",
-                        style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.secondary),
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
+                    items(incompleteTodos, key = { it.id }) {
+                        TodoItem(it, onDelete = {
+                            todoViewModel.deleteTodo(it)
+                        }, onToggle = { todo, isDone ->
+                            todoViewModel.toggleTodo(todo, isDone)
+                        }, modifier = Modifier.animateItem())
+                    }
                 }
 
-                items(incompleteTodos, key = { it.id }) {
-                    TodoItem(it, onDelete = {
-                        todoViewModel.deleteTodo(it)
-                    }, onToggle = { todo, isDone ->
-                        todoViewModel.toggleTodo(todo, isDone)
-                    }, modifier = Modifier.animateItem())
-                }
-            }
+                if (completedTodos.isNotEmpty()) {
+                    item {
+                        Text(
+                            "Completed Tasks",
+                            style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.tertiary),
+                            modifier = Modifier
+                                .padding(top = 16.dp, bottom = 8.dp)
+                                .animateItem()
+                        )
+                    }
 
-            if (completedTodos.isNotEmpty()) {
-                item {
-                    Text(
-                        "Completed Tasks",
-                        style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.tertiary),
-                        modifier = Modifier
-                            .padding(top = 16.dp, bottom = 8.dp)
-                            .animateItem()
-                    )
-                }
-
-                items(completedTodos, key = { it.id }) {
-                    TodoItem(it, onDelete = {
-                        todoViewModel.deleteTodo(it)
-                    }, onToggle = { todo, isDone ->
-                        todoViewModel.toggleTodo(todo, isDone)
-                    }, modifier = Modifier.animateItem())
+                    items(completedTodos, key = { it.id }) {
+                        TodoItem(it, onDelete = {
+                            todoViewModel.deleteTodo(it)
+                        }, onToggle = { todo, isDone ->
+                            todoViewModel.toggleTodo(todo, isDone)
+                        }, modifier = Modifier.animateItem())
+                    }
                 }
             }
         }
